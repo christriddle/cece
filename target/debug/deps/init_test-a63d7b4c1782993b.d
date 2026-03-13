da221d302b10bff37568150526f260f7f19144eb
Chris Riddle <chris.riddle@thetradedesk.com>
2026-03-13 11:15:10 +0000

    feat: add git worktree helpers

---
+++ target/debug/deps/init_test-a63d7b4c1782993b.d
@@ -0,0 +1,5 @@
+/Users/chris.riddle/dev/cece/target/debug/deps/init_test-a63d7b4c1782993b.d: tests/init_test.rs
+
+/Users/chris.riddle/dev/cece/target/debug/deps/init_test-a63d7b4c1782993b: tests/init_test.rs
+
+tests/init_test.rs:
